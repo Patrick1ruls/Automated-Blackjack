99ba968aade7d76b70678d7ae3012a4a5330b8d2
Patrick1ruls <patrickvermillion555@gmail.com>
2019-11-02 21:44:34 -0500

    Created Card Class
    closes issue #5

--- Blackjack.py
+++ Blackjack.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov  2 13:56:00 2019
-
 @author: patrickvermillion
 """
+
+class Card(object):
+    # For playing cards containing both a name (rank and suit) and a value (1 - 11)
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        
+    def __str__(self): # Print card name directly
+        return "%s" % (self.name)
+    
+test_card = Card("K Diamonds", 10)
+
+print("%s = %d" % (test_card, test_card.value))
 
 print("Hello Blackjack!")
