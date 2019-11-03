af8b73afa7b97cdbfb3d7001fcd0e18339f14c61
Patrick1ruls <patrickvermillion555@gmail.com>
2019-11-03 11:11:29 -0600

    Created Shoe Object
    closes #2
    
    Shoe will initialize and shuffle 52 * SHOE_SIZE worth of cards
    
    Included quick tests to verify basic functionality

--- Blackjack.py
+++ Blackjack.py
@@ -4,6 +4,15 @@
 @author: patrickvermillion
 """
 
+from random import shuffle
+
+
+# Define card types through dictionary
+CARDS = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
+SHOE_SIZE = 1
+
+
+"""
 class Card(object):
     # For playing cards containing both a name (rank and suit) and a value (1 - 11)
     def __init__(self, rank, suit, value):
@@ -12,10 +21,99 @@ class Card(object):
         
     def __str__(self): # Print card name directly
         return "%s" % (self.name)
+"""
+class Card(object):
+    # For playing cards containing both a name (rank and suit) and a value (1 - 11)
+    def __init__(self, rank, suit, value):
+        self.rank = rank
+        self.suit = suit
+        self.value = value
+        
+    def __str__(self): # Print card name directly
+        return "%s %s" % (self.rank, self.suit)        
     
-test_card = Card("K", "Diamonds", 10)
+class Shoe(object):
+    # Consists 6 playing decks
+    def __init__(self):
+        self.count = 0
+        self.count_history = []
+        self.ideal_count = {}
+        self.cards = self.initialize_cards()
+        self.initialize_count()
+        
+    # Print out all cards in the shoe
+    def __str__(self):
+        shoe = ""
+        for card in self.cards:
+            shoe += "%s\n" % card
+        return shoe
+    
+    def initialize_cards(self):
+        # Set up the shoe with a full set of 6 playing card decks
+        self.count = 0
+        self.count_history.append(self.count)
+        
+        cards = []
+        for deck in range(SHOE_SIZE): # There must be 6 decks in the shoe
+            for card in CARDS:
+                # Add suits to each card
+                cards.append(Card(card, "Diamonds", CARDS[card]))
+                cards.append(Card(card, "Clubs", CARDS[card]))
+                cards.append(Card(card, "Hearts", CARDS[card]))
+                cards.append(Card(card, "Spades", CARDS[card]))       
+        shuffle(cards)
+        return cards
+    
+    def initialize_count(self):
+        # Track number of cards throught the program
+        for card in CARDS:
+            self.ideal_count[card] = 4 * SHOE_SIZE # 4 of each card * shoe size
+    
+    def deal(self):
+        # Pulls next card off top of the shoe, if shoe is empty then program ends
+        card = self.cards.pop()
+        
+        # Check wether there are any copies of that card left
+        assert self.ideal_count[card.rank] > 0, "Error, no more of that card"
+        self.ideal_count[card.rank] -= 1
+        
+        return card
+        
 
-print("%s = %d" % (test_card, test_card.value))
+# Test to make sure deck is generated properly
+# Test to make sure deck is suffled properly
+# Test to make sure deal functions properly
+test_shoe = Shoe()
+
+print(test_shoe)
+
+for card in test_shoe.cards:
+    print(len(test_shoe.cards))
+    print(test_shoe.deal())
+    
+for card in test_shoe.cards:
+    print(len(test_shoe.cards))
+    print(test_shoe.deal())    
+    
+for card in test_shoe.cards:
+    print(len(test_shoe.cards))
+    print(test_shoe.deal())  
+    
+for card in test_shoe.cards:
+    print(len(test_shoe.cards))
+    print(test_shoe.deal())
+    
+print(len(test_shoe.cards))
+print(test_shoe.deal())
+
+print(len(test_shoe.cards))
+print(test_shoe.deal())
+
+print(len(test_shoe.cards))
+print(test_shoe.deal())
+
+print(len(test_shoe.cards))
+
 
 print("Hello Blackjack!")
 
