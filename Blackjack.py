980592fba359a12ae120c598925d747b33c410d8
Patrick1ruls <patrickvermillion555@gmail.com>
2019-11-10 13:04:19 -0600

    Added Game Over When Shoe Empty Functionality
    closes #36
    Program will also throw away the results of the last round if shoe runs out of cards
    Also changed comments for documentation
    
    Current output:
    Player Hand: 2 Hearts 4 Hearts 3 Hearts 8 Diamonds = 17
    Dealer Hand: 4 Diamonds A Clubs 6 Clubs = 21
    Result: Dealer wins!
    
    Player Hand: 2 Clubs 9 Clubs 7 Clubs = 18
    Dealer Hand: J Diamonds Q Diamonds = 20
    Result: Dealer wins!
    
    Player Hand: 2 Spades Q Clubs Q Spades = 22
    Dealer Hand: 5 Spades = 5
    Result: Dealer wins!
    
    Player Hand: 7 Hearts 3 Spades 9 Hearts = 19
    Dealer Hand: 9 Diamonds 7 Diamonds J Hearts = 26
    Result: Player wins!
    
    Player Hand: 10 Diamonds 10 Clubs = 20
    Dealer Hand: 4 Clubs A Spades J Spades 3 Diamonds A Diamonds 10 Spades = 29
    Result: Player wins!
    
    Player Hand: 7 Spades K Spades = 17
    Dealer Hand: K Diamonds 8 Hearts = 18
    Result: Dealer wins!
    
    Player Hand: 5 Hearts 2 Diamonds K Hearts = 17
    Dealer Hand: J Clubs 4 Spades Q Hearts = 24
    Result: Player wins!
    
    Player Hand: 10 Hearts 8 Spades = 18
    Dealer Hand: K Clubs 3 Clubs 9 Spades = 22
    Result: Player wins!
    
    Player Hand: 6 Diamonds 8 Clubs 6 Spades = 20
    Dealer Hand: A Hearts 5 Clubs 5 Diamonds = 21
    Result: Dealer wins!
    
    Number of games: 10
    
    Player Success: 40.0%
    
    Player Winning Hand => # of times achieved
    21 => 0
    20 => 1
    19 => 1
    18 => 1
    17 => 1
    16 => 0

--- Blackjack.py
+++ Blackjack.py
@@ -6,24 +6,37 @@
 
 from random import shuffle
 
-# Define card types through dictionary
+"""
+Define card types through dictionary
+"""
 CARDS = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
 SHOE_SIZE = 1
+"""
+Variable for triggering when the game is over (No more cards in shoe)
+"""
+GAME_OVER = False
 
 
 class Card(object):
-    # For playing cards containing both a name (rank and suit) and a value (1 - 11)
+    """
+    For playing cards containing both a name (rank and suit) and a value (1 - 11)
+    """
     def __init__(self, rank, suit, value):
         self.rank = rank
         self.suit = suit
         self.value = value
         
-    def __str__(self): # Print card name directly
+    def __str__(self): 
+        """
+        Print card name directly
+        """
         return "%s %s" % (self.rank, self.suit)        
     
     
 class Shoe(object):
-    # Consists 6 playing decks
+    """
+    Consists 6 playing decks
+    """
     def __init__(self):
         self.count = 0
         self.count_history = []
@@ -31,7 +44,9 @@ class Shoe(object):
         self.cards = self.initialize_cards()
         self.initialize_count()
         
-    # Print out all cards in the shoe
+    """
+    Print out all cards in the shoe
+    """
     def __str__(self):
         shoe = ""
         for card in self.cards:
@@ -39,14 +54,21 @@ class Shoe(object):
         return shoe
     
     def initialize_cards(self):
-        # Set up the shoe with a full set of 6 playing card decks
+        """
+        Set up the shoe with a full set of 6 playing card decks
+        """
         self.count = 0
         self.count_history.append(self.count)
         
         cards = []
-        for deck in range(SHOE_SIZE): # There must be 6 decks in the shoe
+        for deck in range(SHOE_SIZE): 
+            """
+            There must be 6 decks in the shoe
+            """
             for card in CARDS:
-                # Add suits to each card
+                """
+                Add suits to each card
+                """
                 cards.append(Card(card, "Diamonds", CARDS[card]))
                 cards.append(Card(card, "Clubs", CARDS[card]))
                 cards.append(Card(card, "Hearts", CARDS[card]))
@@ -55,24 +77,36 @@ class Shoe(object):
         return cards
     
     def initialize_count(self):
-        # Track number of cards throught the program
+        """
+        Track number of cards throught the program
+        """
         for card in CARDS:
             self.ideal_count[card] = 4 * SHOE_SIZE # 4 of each card * shoe size
     
     def deal(self):
-        # Pulls next card off top of the shoe, if shoe is empty then program ends
-        card = self.cards.pop()
-        # Check wether there are any copies of that card left
-        assert self.ideal_count[card.rank] > 0, "Error, no more of that card"
-        self.ideal_count[card.rank] -= 1
-        return card
+        """
+        Pulls next card off top of the shoe, if shoe is empty then program ends
+        """
+        if self.cards:
+            card = self.cards.pop()
+            """
+            Check wether there are any copies of that card left
+            """
+            assert self.ideal_count[card.rank] > 0, "Error, no more of that card"
+            self.ideal_count[card.rank] -= 1
+            return card
+        else:
+            global GAME_OVER 
+            GAME_OVER = True
     
     def length(self):
         return len(self.cards)
     
     
 class Hand(object):
-    # Both player and dealer will have a playing hand
+    """
+    Both player and dealer will have a playing hand
+    """
     _value = 0
     _aces = []
     _soft_aces = 0
@@ -88,7 +122,9 @@ class Hand(object):
     
     @property
     def aces(self):
-        # Returns any aces in hand
+        """
+        Returns any aces in hand
+        """
         self._aces = []
         for card in self.cards:
             if card.rank == "A":
@@ -97,7 +133,9 @@ class Hand(object):
     
     @property
     def soft_aces(self):
-        # Returns any soft aces (Aces valued at 11)
+        """
+        Returns any soft aces (Aces valued at 11)
+        """
         self._soft_aces = 0
         for ace in self.aces:
             if ace.value == 11:
@@ -106,40 +144,61 @@ class Hand(object):
     
     @property
     def value(self):
-        # Will return value of cards in hand
+        """
+        Will return value of cards in hand
+        """
         self._value = 0
-        for card in self.cards:
-            self._value += card.value
-            
-        if self._value > 21 and self.soft_aces > 0:
-            for ace in self.aces:
-                if ace.value == 11:
-                    self._value -= 10
-                    ace.value = 1
-                    if self._value <= 21:
-                        break
-        return self._value
+        if self.cards:
+            for card in self.cards:
+                if card:
+                    self._value += card.value
+                else:
+                    break
+                
+            if self._value > 21 and self.soft_aces > 0:
+                for ace in self.aces:
+                    if ace.value == 11:
+                        self._value -= 10
+                        ace.value = 1
+                        if self._value <= 21:
+                            break
+            return self._value
+        else:
+            global GAME_OVER 
+            GAME_OVER = True
     
     def add_card(self, card):
-        # Add a card to given hand
+        """
+        Add a card to given hand
+        """
         self.cards.append(card)
         
     def soft(self):
-        # Figures out if hand is soft
+        """
+        Figures out if hand is soft
+        """
         if self.soft_aces > 0:
             return True
         else:
             return False
         
     def busted(self):
-        # Check's to see if hand's value goes over 21
-        if self.value > 21:
-            return True
+        """
+        Check's to see if hand's value goes over 21
+        """
+        if self.value == None:
+            global GAME_OVER
+            GAME_OVER = True
         else:
-            return False
+            if self.value > 21:
+                return True
+            else:
+                return False
         
     def length(self):
-        # Determines how many cards are in hand
+        """
+        Determines how many cards are in hand
+        """
         return len(self.cards)
     
     
@@ -157,21 +216,27 @@ class Player(object):
         Player will always hit as long as hand isn't busted 
         and the players hand value is less than 16
         """
-        while not hand.busted() and hand.value < 16:
+        while not hand.busted() and hand.value < 16 and not GAME_OVER:
             self.hit(hand, shoe)
             
     def set_hands(self, new_hand, new_dealer_hand):
-        # Sets new player and dealer hand
+        """
+        Sets new player and dealer hand
+        """
         self.hands = [new_hand]
         self.dealer_hand = new_dealer_hand
         
     def play(self, shoe):
-        # Depends on play_hand method
+        """
+        Depends on play_hand method
+        """
         for hand in self.hands:
             self.play_hand(hand, shoe)
             
     def hit(self, hand, shoe):
-        # Player adds a card to hand from shoe
+        """
+        Player adds a card to hand from shoe
+        """
         card = shoe.deal()
         hand.add_card(card)   
     
@@ -193,15 +258,19 @@ class Dealer(object):
         return self.player_hand
         
     def play(self, shoe, player_final_hand):
-        # Dealer will continue to play until they beat player or bust
-        # Not certain on logic here
+        """
+        Dealer will continue to play until they beat player or bust or not enough cards to play
+        Not certain on logic here
+        """
         if player_final_hand <= 21:
-            while not (self.hand.busted()) and self.hand.value < player_final_hand:  
+            while not (self.hand.busted()) and self.hand.value < player_final_hand and not GAME_OVER:  
                 self.hit(shoe)
 
            
 class Game(object):
-    # Manages the order of games and logging
+    """
+    Manages the order of games and logging
+    """
     def __init__(self):
         self.shoe = Shoe()
         self.player = Player()
@@ -210,7 +279,9 @@ class Game(object):
         self.loses = 0
         self.winning_hands = []
         
-    # Checks who won round
+    """
+    Checks who won the round
+    """
     def won_or_lost(self, hand):
         win = 0
         if hand.busted():
@@ -230,28 +301,31 @@ class Game(object):
         player_hand = Hand([self.shoe.deal(), self.shoe.deal()])
         dealer_hand = Hand([self.shoe.deal()])
 
-        self.player.set_hands(player_hand, dealer_hand)
-        self.dealer.set_hand(dealer_hand)
-        
-        self.player.play(self.shoe)
-        self.dealer_check = self.dealer.check_player_hand(player_hand).value
-        self.dealer.play(self.shoe, self.dealer_check)
-        
-        print("Player Hand: " + str(player_hand) + "= " + str(player_hand.value))
-        print("Dealer Hand: " + str(dealer_hand) + "= " + str(dealer_hand.value))
-                
-        # Keep track of wins/loses
-        for hand in self.player.hands:
-            if self.won_or_lost(hand) == 1:
-                self.wins += 1
-                print("Result: Player wins!\n")
-                self.winning_hands.append(player_hand.value)
-            elif self.won_or_lost(hand) == -1:
-                self.loses += 1
-                print("Result: Dealer wins!\n")
-            else:
-                print("Result: It's a tie...\n")  
-        return self.wins, self.loses
+        if not GAME_OVER:
+            self.player.set_hands(player_hand, dealer_hand)
+            self.dealer.set_hand(dealer_hand)
+            
+            self.player.play(self.shoe)
+            self.dealer_check = self.dealer.check_player_hand(player_hand).value
+            self.dealer.play(self.shoe, self.dealer_check)
+            
+            print("Player Hand: " + str(player_hand) + "= " + str(player_hand.value))
+            print("Dealer Hand: " + str(dealer_hand) + "= " + str(dealer_hand.value))
+                    
+            """
+            Keep track of wins/loses
+            """
+            for hand in self.player.hands:
+                if self.won_or_lost(hand) == 1:
+                    self.wins += 1
+                    print("Result: Player wins!\n")
+                    self.winning_hands.append(player_hand.value)
+                elif self.won_or_lost(hand) == -1:
+                    self.loses += 1
+                    print("Result: Dealer wins!\n")
+                else:
+                    print("Result: It's a tie...\n")  
+            return self.wins, self.loses
     
     def get_wins(self):
         return self.wins
@@ -267,7 +341,9 @@ class Game(object):
         self._17 = 0
         self._16 = 0
         
-        # Add up winning values
+        """
+        Add up winning values
+        """
         for hand in winning_hands:
             if hand == 21:
                 self._21 += 1
@@ -282,7 +358,9 @@ class Game(object):
             if hand == 16:
                 self._16 += 1
                 
-        # Print them out
+        """
+        Print winning values out
+        """
         print("Player Winning Hand => # of times achieved")
         print("21 => " + str(self._21))
         print("20 => " + str(self._20))
@@ -299,8 +377,7 @@ if __name__ == "__main__":
     game = Game()
     number_of_games = 0
     
-    while game.shoe.length() > 2:
-    #for games in range(5):
+    while not GAME_OVER:
         game.play_round()
         number_of_games += 1
         
